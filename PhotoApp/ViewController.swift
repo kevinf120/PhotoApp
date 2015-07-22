10caa320938f0826ce505c8b369831f734aa68c3
gorilla04 <kevkev0123@gmail.com>
2015-07-22 10:14:12 -0700

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Kevin Fang on 7/22/15.
+//  Copyright (c) 2015 Kevin Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
