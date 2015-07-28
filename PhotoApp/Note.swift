a1ac3d2ffada79ab1ca01fa670c3850da2ac214a
gorilla04 <kevkev0123@gmail.com>
2015-07-28 16:28:45 -0700

    no message

---
+++ PhotoApp/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  PhotoApp
+//
+//  Created by Kevin Fang on 7/28/15.
+//  Copyright (c) 2015 Kevin Fang. All rights reserved.
+//
+
+import Cocoa
+
+class Note: UITableViewCell {
+
+}
---
+++ PhotoApp/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  PhotoApp
+//
+//  Created by Kevin Fang on 7/27/15.
+//  Copyright (c) 2015 Kevin Fang. All rights reserved.
+//
+
+import Cocoa
+
+class TableViewController: UITableViewCell {
+
+}
